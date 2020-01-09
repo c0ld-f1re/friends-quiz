fccbc4f286e38d08311398eab294208260e9c3dc
c0ld-f1re <59137709+c0ld-f1re@users.noreply.github.com>
2020-01-09 20:52:10 +0530

    Create Post.java

---
+++ Post.java
@@ -0,0 +1,49 @@
+package com.example.friends_4ever;
+
+public class Post {
+    private int id;
+    private  String question;
+    private int ans;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String image;
+    private String character;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public int getAns() {
+        return ans;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public String getOption3() {
+        return option3;
+    }
+
+    public String getOption4() {
+        return option4;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getCharacter() {
+        return character;
+    }
+}
